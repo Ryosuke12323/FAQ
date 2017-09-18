7266023df562d1b42f9ddee0726f5eb503cbd0c0
水田涼介 <basket11115555@gmail.com>
2017-09-18 15:37:33 +0900

    defined actions to qas_controller

--- app/controllers/qas_controller.rb
+++ app/controllers/qas_controller.rb
@@ -2,4 +2,19 @@ class QasController < ApplicationController
   def index
     @qas = Qa.all
   end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def update
+  end
 end
