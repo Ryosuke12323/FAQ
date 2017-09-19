dc2c86c66392c346cef02ed0295ae970e0f08d53
水田涼介 <basket11115555@gmail.com>
2017-09-19 17:59:51 +0900

    Implement SlideDown/Up function

--- app/assets/javascripts/fadein_answer.js
+++ app/assets/javascripts/fadein_answer.js
@@ -2,11 +2,11 @@ $(document).on("turbolinks:load", function(){
   $(".question").on("click", function(){
     if($(this).hasClass("active")){
       $(this).removeClass("active")
-      $(this).next().fadeOut();
+      $(this).next().slideUp(150);
     }else{
       $(this).parent().find(".active").next().fadeOut("fast");
       $(this).addClass("active")
-      $(this).next().fadeIn();
+      $(this).next().slideDown(150);
     }
  })
 })
